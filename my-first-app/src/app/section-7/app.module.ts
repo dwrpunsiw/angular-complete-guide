d52fa78b2e86443500a04d4156a8a7c9f5536190
adl.wisnup <wisnuprsj@gmail.com>
2022-02-20 00:34:30 +0700

    finished section 07

--- my-first-app/src/app/section-7/app.module.ts
+++ my-first-app/src/app/section-7/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
+// Components
 import { AppComponent } from './app.component';
+
+// Directive
+import { UnlessDirective } from './unless.directive';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
