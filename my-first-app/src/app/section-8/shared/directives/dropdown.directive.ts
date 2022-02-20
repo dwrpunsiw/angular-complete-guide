d32a6828bcf9cdfccd3677c42e944e6acfac7eab
adl.wisnup <wisnuprsj@gmail.com>
2022-02-20 13:13:47 +0700

    finished section 9

--- my-first-app/src/app/section-8/shared/directives/dropdown.directive.ts
+++ my-first-app/src/app/section-8/shared/directives/dropdown.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   Renderer2,
   ViewContainerRef,
@@ -10,15 +11,18 @@ import {
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  isOpen = false;
-  @HostListener('click') onclick(eventData: Event) {
-    if (!this.isOpen) {
-      this.renderer.addClass(this.elementRef.nativeElement, 'open');
-    } else {
-      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
-    }
+  //   @HostListener('click') onclick(eventData: Event) {
+  //     if (!this.isOpen) {
+  //       this.renderer.addClass(this.elementRef.nativeElement, 'open');
+  //     } else {
+  //       this.renderer.removeClass(this.elementRef.nativeElement, 'open');
+  //     }
+  //     this.isOpen = !this.isOpen;
+  //   }
+
+  //   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+  @HostBinding('class.open') isOpen: boolean = false;
+  @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
-
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 }
