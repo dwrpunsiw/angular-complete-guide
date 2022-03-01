fcf2bae912a2e267bb7e2515287f64c676acbff8
wisnuprsj <wisnuprsj@gmail.com>
2022-03-02 00:01:54 +0700

    finished section 17

--- my-first-app/src/main.ts
+++ my-first-app/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/section-16/app.module';
+import { AppModule } from './app/section-17/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
