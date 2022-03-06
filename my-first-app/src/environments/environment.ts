c499ec65857f5fccfb9893b2ddcd90c207debab9
wisnuprsj <wisnuprsj@gmail.com>
2022-03-06 13:02:06 +0700

    finished section 20

--- my-first-app/src/environments/environment.ts
+++ my-first-app/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FIREBASE_AUTH_URL: 'https://identitytoolkit.googleapis.com/v1/accounts',
+  FIREBASE_API_KEY: 'AIzaSyB2r288wNaRNBGo-qV-MCpB15wY8Nihru4',
+  FIREBASE_RECIPE_URL:
+    'https://ng-complete-guide-25b30-default-rtdb.asia-southeast1.firebasedatabase.app',
 };
 
 /*
