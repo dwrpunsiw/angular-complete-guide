14951343b50f6d81e4a77c8516264b957f6b0493
wisnuprsj <wisnuprsj@gmail.com>
2022-03-06 22:58:24 +0700

    finished section 23

--- my-first-app/src/environments/environment.prod.ts
+++ my-first-app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  FIREBASE_AUTH_URL: 'https://identitytoolkit.googleapis.com/v1/accounts',
+  FIREBASE_API_KEY: 'AIzaSyB2r288wNaRNBGo-qV-MCpB15wY8Nihru4',
+  FIREBASE_RECIPE_URL:
+    'https://ng-complete-guide-25b30-default-rtdb.asia-southeast1.firebasedatabase.app',
 };
